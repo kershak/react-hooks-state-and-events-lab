342ef255922b43dd44d0208640204ea02724b2ef
Nills <yakusaz17@gmail.com>
2024-09-07 13:10:03 -0400

    completed

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectCategory, setSelectCategory] = useState("All");
+
+  function handleCategorySelect(event) {
+    setSelectCategory(event.target.value);
+  }
+
+  const itemToShow = items.filter((item)=> {
+    if (selectCategory === "All") {
+      return true;
+    } else {
+      return item.category === selectCategory;
+    }  
+  });
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCategorySelect}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +27,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemToShow.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
